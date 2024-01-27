4ab689e07b85801087fb9c46a5e3f3447f098762
kenscl <kenschlesiger@gmail.com>
2024-01-27 18:59:26 +0100

    papaer

--- compile.sh
+++ compile.sh
@@ -4,4 +4,3 @@ latex termpaper.tex
 latex termpaper.tex
 dvipdfm termpaper.dvi
 rm termpaper.aux termpaper.blg termpaper.fdb_latexmk termpaper.log termpaper.out termpaper.ps termpaper.dvi termpaper.fls termpaper.org.tex
-zathura termpaper.pdf &
